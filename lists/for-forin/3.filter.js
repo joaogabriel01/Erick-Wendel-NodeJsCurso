ccc9f08476eb46350db86b56e759837800e3028b
Joao Azevedo <joaogfazevedo@gmail.com>
2022-05-06 20:24:24 -0300

    feat: implementação filter

--- lists/for-forin/3.filter.js
+++ lists/for-forin/3.filter.js
@@ -2,8 +2,9 @@ const { obterPessoas } = require('./service');
 
 Array.prototype.meuFilter(function (callback) {
     const lista = [];
-    for(item in this) {
-        const result = callback(item, this);
+    for(index in this) {
+        const item = this[index];
+        const result = callback(item, index,this);
         if(!result) continue;
         lista.push(item);
     }
@@ -14,11 +15,13 @@ async function main() {
     try{
         const { results } = await obterPessoas('a');
 
-        const familiaLars = results.filter( (item) =>  {
-             const result = item.name.toLowerCase().indexOf('lars') !== -1 ;
-             return result;
-        });
+        // const familiaLars = results.filter( (item) =>  {
+        //      const result = item.name.toLowerCase().indexOf('lars') !== -1 ;
+        //      return result;
+        // });
 
+        // const names = familiaLars.map(pessoa => pessoa.name);
+        const familiaLars = result.meuFilter((item, index, lista) => item.name.toLowerCase().indexOf('lars') != -1)
         const names = familiaLars.map(pessoa => pessoa.name);
         console.log(names);
     } catch (error) {
